6effeb6815fc0b0ba95ea494834d3b3b3401f166
Aikemi <dhxia@snut.edu.cn>
2025-06-10 17:53:33 +0800

    Initial commit: Project setup with renv

--- R/functions.R
+++ R/functions.R
@@ -15,3 +15,12 @@ plot_model <- function(model, data) {
     geom_abline(intercept = model[1], slope = model[2]) +
     theme_gray(24) # Increased the font size.
 }
+
+# KM 生存分析
+
+# 单因素 cox
+
+# 多因素 cox
+
+# 数据集划分
+# 
